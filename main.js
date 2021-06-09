955565f07a20a0ba61f2fe3fbe4f9c7987ff0a28
czg-es <83419630+czg-es@users.noreply.github.com>
2021-06-10 00:27:14 +0200

    added reference to frame in main.js

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const frame  = document.getElementById('iframe');
 const frame1_contentWindow = frame.contentWindow;
 const frame1_Window = frame.window;
 const frame1_doc = frame.contentDocument ;
-        
+
+var iFrameWin; //reference to the window object, sent from iframe
 
 let parent_bg = getComputedStyle(document.documentElement).getPropertyValue('--bg_color');
 let parent_dark = getComputedStyle(document.documentElement).getPropertyValue('--dark_color');
 let parent_text = getComputedStyle(document.documentElement).getPropertyValue('--txt_color');
 
-let child_bg = getComputedStyle(frame1_contentWindow.document.documentElement).getPropertyValue('--bg_color');
-let child_dark = getComputedStyle(frame1_contentWindow.document.documentElement).getPropertyValue('--dark_color');
-let child_text = getComputedStyle(frame1_contentWindow.document.documentElement).getPropertyValue('--txt_color');
+let child_bg = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--bg_color');
+let child_dark = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--dark_color');
+let child_text = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--txt_color');
 
 console.log('SELF');
 console.log(parent_bg,parent_dark,parent_text);
