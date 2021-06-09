2e57adea4219c6d766e9502fb74033fc4c06cf31
czg-es <83419630+czg-es@users.noreply.github.com>
2021-06-10 00:31:00 +0200

    added function get_values

--- main.js
+++ main.js
@@ -6,15 +6,17 @@ const frame1_doc = frame.contentDocument ;
 
 var iFrameWin; //reference to the window object, sent from iframe
 
-let parent_bg = getComputedStyle(document.documentElement).getPropertyValue('--bg_color');
-let parent_dark = getComputedStyle(document.documentElement).getPropertyValue('--dark_color');
-let parent_text = getComputedStyle(document.documentElement).getPropertyValue('--txt_color');
+function get_values(){
+  let parent_bg = getComputedStyle(document.documentElement).getPropertyValue('--bg_color');
+  let parent_dark = getComputedStyle(document.documentElement).getPropertyValue('--dark_color');
+  let parent_text = getComputedStyle(document.documentElement).getPropertyValue('--txt_color');
 
-let child_bg = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--bg_color');
-let child_dark = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--dark_color');
-let child_text = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--txt_color');
+  let child_bg = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--bg_color');
+  let child_dark = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--dark_color');
+  let child_text = getComputedStyle(iFrameWin.document.documentElement).getPropertyValue('--txt_color');
 
-console.log('SELF');
-console.log(parent_bg,parent_dark,parent_text);
-console.log('CHILD');
-console.log(child_bg,child_dark,child_text);
+  console.log('SELF');
+  console.log(parent_bg,parent_dark,parent_text);
+  console.log('CHILD');
+  console.log(child_bg,child_dark,child_text);
+  }
